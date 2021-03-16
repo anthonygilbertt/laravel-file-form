af5e313a56881d4d6af8a591d0fc3a26c8e35032
Anthony <anthony.gilbert93@gmail.com>
2021-03-16 17:11:33 -0500

    Added UI for list of files uploaded

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,2 +1,17 @@
+<body>
 <?php
-    dd($files);
+// dd($files);
+// print_r($files);
+// foreach($files as $file){
+//     echo $file . "<br>";
+// }
+?>
+
+@foreach ($files as $file)
+    <p>{{ $file }}</p>
+@endforeach
+    <!-- /* Use a Blade ForEach declarative to loop through the array and create downloadable links.*/
+    /* */ -->
+
+
+</body>
